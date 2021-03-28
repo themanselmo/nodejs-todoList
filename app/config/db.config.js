af53762a32305927bbafa45ec7549983f45ec98b
Max Anselmo <itmanselmo@gmail.com>
2021-03-28 13:11:07 -0400

    reworked file structure, and added the first controller to begin interacting with the postgres db

---
+++ app/config/db.config.js
@@ -0,0 +1,22 @@
+/*
+ First five parameters are for PostgreSQL connection.
+    pool is optional, it will be used for Sequelize connection pool configuration:
+
+    max: maximum number of connection in pool
+    min: minimum number of connection in pool
+    idle: maximum time, in milliseconds, that a connection can be idle before being released
+    acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error
+*/
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "123",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
---
+++ server.js
@@ -0,0 +1,36 @@
+/*
+ import express, body-parser and cors modules:
+
+    Express is for building the Rest apis
+    body-parser helps to parse the request and create the req.body object
+    cors provides Express middleware to enable CORS with various options.
+*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
