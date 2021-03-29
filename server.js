77898099afcbcb1940538a6f6c0909ce04eea49f
Max Anselmo <itmanselmo@gmail.com>
2021-03-29 12:14:57 -0400

    finished building the backend of the application

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+//  include routes
+require("./app/routes/turorial.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
