c1f7f6e351735e8b86e961ab06824551d4ee9ce9
Max Anselmo <itmanselmo@gmail.com>
2021-03-29 13:06:51 -0400

    finished implementation of the front end and the application is up and running on my local postgres test server

---
+++ pg-backend/app/config/db.config.js
@@ -0,0 +1,23 @@
+/*
+ First five parameters are for PostgreSQL connection.
+    pool is optional, it will be used for Sequelize connection pool configuration:
+
+    max: maximum number of connection in pool
+    min: minimum number of connection in pool
+    idle: maximum time, in milliseconds, that a connection can be idle before being released
+    acquire: maximum time, in milliseconds, that pool will try to get connection before throwing error
+*/
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "skueeke99",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+  
